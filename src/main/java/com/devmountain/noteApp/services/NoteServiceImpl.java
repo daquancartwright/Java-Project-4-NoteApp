135aa74051892b472027cecdd0f6d66e0d58c816
DaQuan Cartwright <daquancartwright@outlook.com>
2023-09-14 10:49:17 -0700

    fixed bug

--- src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
+++ src/main/java/com/devmountain/noteApp/services/NoteServiceImpl.java
@@ -7,12 +7,14 @@ import com.devmountain.noteApp.repositories.NoteRepository;
 import com.devmountain.noteApp.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class NoteServiceImpl implements NoteService {
     @Autowired
     private UserRepository userRepository;
